da9733b6582c6ff5909929a36e1c63162c0838e9
F1lterEr <41ilter.er@gmail.com>
2022-12-19 11:37:28 +0300

    commit3

--- src/main/java/com/example/warehousecheck/Cargo.java
+++ src/main/java/com/example/warehousecheck/Cargo.java
@@ -1,9 +1,10 @@
 package com.example.warehousecheck;
 
+//Данные о базе данных
 public class Cargo {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "Iklipop321KLP";
+    protected String dbPass = "123";
     protected String dbName = "cargobd";
 }
